f7f8e8de845967b51ccd38fd358d6f084920d725
XuYijie <1119461672@qq.com>
2023-08-29 21:18:47 +0800

    增加笔记

--- EmployeeManagement/src/main/java/com/tiantian/employeemanagement/controller/EmployeeController.java
+++ EmployeeManagement/src/main/java/com/tiantian/employeemanagement/controller/EmployeeController.java
@@ -3,7 +3,6 @@ package com.tiantian.employeemanagement.controller;
 import com.tiantian.employeemanagement.entity.Employee;
 import com.tiantian.employeemanagement.mapper.EmployeeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,26 +12,19 @@ import java.util.List;
  * @date 2023/7/22 14:15
  * @description 员工操作相关API
  */
-@Controller
+@RestController
 @RequestMapping("/employee")
 public class EmployeeController {
     @Autowired
     private EmployeeMapper employeeMapper;
 
-
     @GetMapping("/getEmployeeList")
-    @ResponseBody
     public List<Employee> getEmployeeList(Employee employee) {
-        List<Employee> employeeList = employeeMapper.searchEmployeeList(employee);
-        return employeeList;
+        return employeeMapper.searchEmployeeList(employee);
     }
 
-
-
-    @PostMapping("/delEmployee2")
-    @ResponseBody
-    public Employee delEmployee2(@RequestBody Employee employee) {
-        System.out.println(employee);
+    @PostMapping("/delEmployee")
+    public Employee delEmployee(@RequestBody Employee employee) {
         employee.setIsDelete(true);
         employeeMapper.update(employee);
         return employee;
@@ -44,9 +36,8 @@ public class EmployeeController {
      * @param id id
      * @return 跳转的页面文件名
      */
-    @GetMapping("/goEditEmployee2/{id}")
-    @ResponseBody
-    public Employee goEditEmployee2(@PathVariable Integer id) {
+    @GetMapping("/goEditEmployee/{id}")
+    public Employee goEditEmployee(@PathVariable Integer id) {
         return employeeMapper.getById(id);
     }
 
